a7d3d01d688efe7dd22d7ca9ff3c47fa150462cc
Tanish Padvekar <tanishpadvekar@gmail.com>
2025-03-21 21:29:34 +0530

    Initial commit

---
+++ app/_data/Lookup.jsx
@@ -0,0 +1,6 @@
+export default {
+    HeroHeading: 'AI Logo Maker',
+    HeroSubheading: 'Perfect Logos for Apps, Businesses, Websites',
+    HeroDesc: 'Craft unique and professional logos effortlessly with AI-powered tool.Perfect for apps, businesses,websites, and more!',
+    InputTitlePlaceholder:'Enter your Logo Name'
+}
